06f16df76a76f3a5ac5aa467013c85cc9a3b25f5
mohitraghav1318 <mohitraghav1318.com>
2025-10-19 14:49:06 +0530

    hosting

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./src/app');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
