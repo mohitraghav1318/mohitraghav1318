a6b18c245bb84bb677bb87fdc02b5e5754f3085d
mohitraghav1318 <mohitraghav1318.com>
2025-10-26 20:05:42 +0530

    update seo

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 
 // Middleware
 app.use(logger);
+app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
